2bf91c15f60d066bfe910b3239cf1bd3faa63b6c
mohammadIsamil <ismailalam901@gmail.com>
2023-07-07 17:29:48 +0600

    tailwind

--- src/auth/Registated.js
+++ src/auth/Registated.js
@@ -12,15 +12,17 @@ const Registered = () => {
   };
 
   return (
-    <div className="p-4">
-      <h2 className="text-2xl font-bold mb-4">Registered</h2>
-      <p className="mb-2">Welcome, {username}!</p>
-      <button
-        onClick={handleProductListView}
-        className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
-      >
-        Product List View
-      </button>
+    <div className="flex justify-center items-center h-screen">
+      <div className="p-4">
+        <h2 className="text-2xl font-bold mb-4">Registered</h2>
+        <p className="mb-2">Welcome, {username}!</p>
+        <button
+          onClick={handleProductListView}
+          className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+        >
+          Product List View
+        </button>
+      </div>
     </div>
   );
 };
