57c1b5c9fe5c3360262b4aa6680ac310197d8da8
mohammadIsamil <ismailalam901@gmail.com>
2023-07-07 09:21:40 +0600

    admin section

---
+++ src/admin/AddProductes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddProductes = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AddProductes
---
+++ src/admin/AdminNav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Container, Row, Col} from "reactstrap"
+
+const AdminNav = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AdminNav
